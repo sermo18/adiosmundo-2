21c30532cc00a3130cca6711e8a47d48fb18b7b6
Jesus <suso2asi@gmail.com>
2019-10-03 18:28:26 +0200

    Esperimento con colores2

--- adiosmundo.java
+++ adiosmundo.java
@@ -4,8 +4,9 @@ public class adiosmundo {
 	public static void main(String[] args) {
 		String amarillo = "\33[33m";
 		String verde = "\33[32m";
-		System.out.println(amarillo + "adios mundo");
-		System.out.println(verde + "pero volvere");
+		String reset = "\33[0m";
+		System.out.println(amarillo + "adios mundo" + reset);
+		System.out.println(verde + "pero volvere" + reset);
 	}
 
 }
